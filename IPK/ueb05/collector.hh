6846d2ba26045a01ae559cc0aefc551ff9224971
Jan <MapManagement@protonmail.com>
2024-01-14 21:24:29 +0100

    IPK: fix A1 of ueb05

--- IPK/ueb05/collector.hh
+++ IPK/ueb05/collector.hh
@@ -1,27 +1,56 @@
 #include <string>
 #include <vector>
 template <typename T> class Collector {
-  std::vector<T> vec;
+    private:
+    T sum;
+    T adder;
 
-public:
-  Collector(T firstItem, T secondItem) : vec() {
-    vec.push_back(firstItem);
-    vec.push_back(secondItem);
-  }
+    public:
+    // Konstruktor
+    Collector(T initialSum, T initialAdder)
+        : sum(initialSum), adder(initialAdder) {}
 
-  void add(T item) { vec.push_back(item); }
-
-  std::string digest() {
-    std::string output = "";
-
-    for (int i = 0; i < vec.size(); ++i) {
-      output += std::to_string(vec[i]);
-
-      if (i != vec.size() - 1) {
-        output.append(", ");
-      }
+    // Methode zum Hinzufügen eines Werts zu sum und adder
+    void add(T value) {
+        sum += adder;
+        sum += value;
     }
 
-    return output;
-  }
+    // Methode zum Abrufen des aktuellen Werts von sum
+    T digest() const { return sum; }
+};
+
+template <> class Collector<std::string> {
+    private:
+    std::string separator;
+    std::vector<std::string> stringVector;
+
+    public:
+    // Konstruktor
+    Collector(std::string firstElement, std::string separator)
+        : stringVector(), separator(separator) {
+        stringVector.push_back(firstElement);
+    }
+
+    // Methode zum Hinzufügen eines Wertes zu Vector
+    void add(std::string value) { stringVector.push_back(value); }
+
+    // Methode zum Abrufen der separierten Ausgabeliste
+    std::string digest() const {
+        if (stringVector.size() == 0) {
+            return "";
+        }
+
+        std::string output = "";
+
+        for (int i = 0; i < stringVector.size(); ++i) {
+            output += stringVector[i];
+
+            if (i != stringVector.size() - 1) {
+                output += separator;
+            }
+        }
+
+        return output;
+    }
 };
